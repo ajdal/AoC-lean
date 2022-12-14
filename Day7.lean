adb373cd590ab938d7c84a0ad6e7c0fd218f3c23
cilinder <jure.taslak@gmail.com>
2022-12-14 16:40:26 +0100

    Finished Day7 part 1

--- Day7.lean
+++ Day7.lean
@@ -5,16 +5,51 @@ inductive Directory where
   | file : Nat → String → Directory
   | directory : String → List Directory → (size : Nat := 0) → Directory
 
-open Directory in
+open Directory
+
 partial def dir2String (level : String :="") : Directory → String
   | file size name => s!"\n{level}- {name} (file, size={size})"
   | directory name contents size => 
     s!"\n{level}- {name} (dir, size={size}) {contents.map (fun c => (dir2String (level++"  ") c) )}"
 
 instance : ToString Directory where
-  toString := dir2String 
+  toString := dir2String
 
-open Directory in
+def insertDirectory : Directory → Directory → Directory
+  | newDir, directory name contents size => directory name ([newDir] ++ contents) size
+  | _, f => f
+
+def getName : Directory → String
+  | file _ name => name
+  | directory name _ _ => name
+
+def insertAt (root: Directory) : Directory → List String → Option Directory
+  | d, [] => insertDirectory d root
+  | d, h :: t =>
+    match root with
+    | file _ _ => none
+    | directory rootName rootContents rootSize =>
+      let mbDir := rootContents.find? (fun c =>
+        match c with
+        | file _ _ => false
+        | directory childName _ _ => childName == h
+      )
+      match mbDir with
+      | none => none
+      | some (file _ _) => none
+      | some childDir =>
+        let mbNewDir := insertAt childDir d t
+        match mbNewDir with
+        | none => none
+        | some newDir =>
+          let temp := rootContents.filter (fun c =>
+            match c with
+            | file _ _ => true
+            | directory cName _ _ => cName != (getName childDir)
+          )
+          let newContents := [newDir] ++ temp
+          directory rootName newContents rootSize
+
 def fs := directory "/" [
   directory "a" [
     directory "e" [
@@ -34,23 +69,32 @@ def fs := directory "/" [
   ]
 ]
 
-#eval fs
+def fs2 := directory "/" [
+  directory "a" [
+    directory "e" [
+      file 584 "i.txt",
+      directory "x" [
+        directory "y" []
+      ]
+    ]
+  ]
+]
 
+def fs3 := insertAt fs2 (directory "y" []) ["a", "e", "x"]
+
+#eval fs3
 
 instance : Inhabited Directory where
   default := Directory.directory "" []
 
-
-open Directory in
 partial def getSizes : Directory → Nat × Directory
 | file size name => (size, file size name)
 | directory name contents _ =>
-  let temp := contents.map (fun c => getSizes c)
-  let newDirs := temp.map (fun ⟨ _, d ⟩ => d)
-  let totalSize := temp.foldl (fun sum ⟨ size, _ ⟩  => sum + size) 0
+  let sizes := contents.map (fun c => getSizes c)
+  let newDirs := sizes.map (fun ⟨ _, d ⟩ => d)
+  let totalSize := sizes.foldl (fun sum ⟨ size, _ ⟩  => sum + size) 0
   (totalSize, directory name newDirs totalSize)
 
-open Directory in
 partial def sumSmaller : Directory → Nat
 | file _ _ => 0
 | directory _ contents size => 
@@ -67,74 +111,52 @@ partial def readLines (stream : IO.FS.Stream) : IO (List String) := do
     let rest ← readLines stream
     return [line.dropRightWhile Char.isWhitespace] ++ rest
 
-open Directory in
 def parseInput : (List String) → Directory := fun lines =>
-  let rec helper (currDir: Directory) (path: List Directory) : List String → Directory
-  | [] => currDir
+  let rec helper (root: Directory) (path: List String) : List String → Directory
+  | [] => root
   | line::lines =>
     let parts := line.splitOn " "
     match parts[0]? with
-    | none => currDir
+    | none => root
     | some "$" =>
       match parts[1]? with
-      | none => currDir
-      | some "cd" => 
+      | none => root
+      | some "cd" =>
         match parts[2]? with
-        | none => currDir
-        | some ".." => 
-          match path with
-          | [] => currDir
-          | _ :: [] => currDir
-          | _ :: parent :: rest => helper parent ([parent] ++ rest) lines
-        | some dirName => 
-          let newDir := directory dirName []
-          match currDir with
-          | directory name contents size =>
-            let currDir' := directory name ([newDir] ++ contents) size
-            if contents.length > 0 then
-              let newPath := [newDir] ++ [currDir'] ++ contents.tail!
-              helper newDir newPath lines
-            else
-              let newPath := [newDir] ++ [currDir']
-              helper newDir newPath lines
-          | _ => currDir
-      | some "ls" => helper currDir path lines
-      | _ => currDir
+        | none => root
+        | some ".." => helper root path.dropLast lines
+        | some dirName => helper root (path ++ [dirName]) lines
+      | some "ls" => helper root path lines
+      | _ => root
     | some "dir" =>
       match parts[1]? with
-      | none => currDir
-      | some dirName => 
-        match currDir with
-        | file _ _ => currDir
-        | directory name contents size =>
-          let currDir' := directory name ([directory dirName []] ++ contents) size
-          if contents.length > 0 then
-            let newPath := [currDir'] ++ contents.tail!
-            helper currDir' newPath lines
-          else
-            let newPath := [currDir']
-            helper currDir' newPath lines
-    | some fSize => 
+      | none => root
+      | some dirName =>
+        match insertAt root (directory dirName []) path with
+        | none => root
+        | some newRoot => helper newRoot path lines
+    | some fSize =>
       match parts[1]? with 
-      | none => currDir
-      | some fName => 
-        match currDir with
-        | file _ _ => currDir
-        | directory name contents size =>
-          let currDir' := directory name ([file fSize.toNat! fName] ++ contents) size
-          if contents.length > 0 then
-            let newPath := [currDir'] ++ contents.tail!
-            helper currDir' newPath lines
-          else
-            let newPath := [currDir']
-            helper currDir' newPath lines
+      | none => root
+      | some fName =>
+        match String.toNat? fSize with
+        | none => root
+        | some size =>
+          match insertAt root (file size fName) path with
+          | none => root
+          | some newRoot => helper newRoot path lines
 
   let root := directory "/" []
-  helper root [root] lines.tail!
+  let newRoot := helper root [] lines.tail!
+  newRoot
 
 
-def testIn := ["$ cd /", "$ ls", "dir a", "1000 b.txt"]
-#eval parseInput testIn
+def testIn := ["$ cd /", "$ ls", "dir a", "1000 b.txt", 
+    "200 c.txt", "dir d", "$ cd a", "$ ls", "dir e", "1342 x.txt", "$ cd e", "$ ls", "584 i.txt", "$ cd ..", "$ cd ..", "$ cd d",
+    "$ ls", "4060174 j"
+  ]
+-- def testIn := ["$ cd /", "$ ls", "dir a", "1000 b.txt", "dir d", "$ cd a", "$ ls", "dir e", "$ cd e", "$ ls", "200 lal.txt", "$ cd ..", "$ ls", "100 c.txt"]
+-- #eval parseInput testIn
 
 def runDay : IO Unit := do
   let stdin ← IO.getStdin
@@ -142,6 +164,6 @@ def runDay : IO Unit := do
   let fs := parseInput lines
 
   let stdout ← IO.getStdout
-  stdout.putStrLn s!"{fs}"
+  stdout.putStrLn s!"{sumSmaller (getSizes fs).snd}"
 
 end Day7
