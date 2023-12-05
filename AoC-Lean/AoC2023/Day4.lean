fa91b92d83293c08973d3d2c7d7b474e8d4dabc5
ajdaL <ajda.lampe@gmail.com>
2023-12-05 22:53:11 +0100

    Day 4 2023 🎉

--- AoC-Lean/AoC2023/Day4.lean
+++ AoC-Lean/AoC2023/Day4.lean
@@ -43,15 +43,28 @@ def intersection : Card × Card → List Nat := fun (winning, mine) =>
   let s2 : Set Nat := Set.mk mine
   (s1 ∩ s2).data
 
+/-- Examples:
+  [1,2,3,4,5] 2 3 => [3, 4, 5, 4, 5]
+  [1,2,3,4,5] 3 7 => [4, 5, 6, 7, 8]
+-/
+def increaseNextK (n k : Nat) : List Nat → List Nat := fun nums => match k, nums with
+  | 0, nums => nums
+  | _, [] => []
+  | k + 1, num::nums => [num + n] ++ increaseNextK n k nums
+
+def totalCards (wins : List Nat) : List Nat → Nat := fun cards => match wins, cards with
+  | [], _ => 0
+  | _, [] => 0
+  | w :: wins, c :: cards =>
+    let next := increaseNextK c w cards
+    c + totalCards wins next
+
 def runDay : List String → String := fun ls =>
-  let sum := List.foldl (fun acc s =>
-    let cards := parseCards s
-    let numWinning := (intersection cards).length
-    if numWinning > 0 then
-      acc + 2 ^ (numWinning - 1)
-    else
-      acc
-  ) 0 ls
-  s!"Part 1: {sum}"
+  let cards := ls.map parseCards
+  let winning := cards.map (List.length ∘ intersection)
+  let sum := winning.foldl (fun acc nw => if nw > 0 then acc + 2 ^ (nw - 1) else acc) 0
+  let initial := winning.map (fun _ => 1)
+  let total := totalCards winning initial
+  s!"Part 1: {sum}¬Part 2: {total}"
 
 end Day4
