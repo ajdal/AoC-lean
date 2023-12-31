64243d9a0ba17da8199af6b747fe5bd928292531
ajdaL <ajda.lampe@gmail.com>
2023-12-31 18:02:12 +0100

    Day 13 2023 - Part 2 🎉

--- AoC-Lean/AoC2023/Day13.lean
+++ AoC-Lean/AoC2023/Day13.lean
@@ -25,32 +25,42 @@ def Tile.ofChar : Char → Tile
   | '.' => .Ash
   | _ => panic! "Unknown tile type"
 
+def arrayDistance (x y : Array Tile) : Nat :=
+  let xy := Array.zip x y
+  Array.foldl (fun acc (xi, yi) => if xi == yi then acc else 1 + acc) 0 xy
+
+instance : HSub (Array Tile) (Array Tile) Nat where
+  hSub := arrayDistance
+
 def checkRowSymmetry : List (Array Tile) → List (Array Tile) → Bool
   | h1 :: t1, h2 :: t2 => h1 == h2 && checkRowSymmetry t1 t2
   | _, _ => true
 
+def symmetryDistance : List (Array Tile) → List (Array Tile) → Nat
+  | [], [] => 0
+  | [], _ => 0
+  | _, [] => 0
+  | h1 :: t1, h2 :: t2 => (h1 - h2) + symmetryDistance t1 t2
 
-def findRowSymmetry : Grid Tile → Option Nat := fun grid =>
-  let rec helper (acc : List (Array Tile)) : List (Array Tile) → Option Nat
-    | [] => none
-    | r1 :: r2 :: rws =>
-      -- dbg_trace "{acc} {r1} {r2} {rws}"
-      -- dbg_trace "{r1 == r2}"
-      -- dbg_trace "{checkRowSymmetry acc rws}"
-      if r1 == r2 && checkRowSymmetry acc rws then
-        some (acc.length + 1)
-      else
-        helper (r1 :: acc) (r2 :: rws)
-    | _ => none
+def findRowSymmetry : Grid Tile → List Nat := fun grid =>
+  let rec helper (acc : List (Array Tile)) : List (Array Tile) → List Nat
+    | [] => []
+    | _ :: [] => []
+    | r :: rws =>
+      let acc' := r :: acc
+      let dist := symmetryDistance acc' rws
+      dist :: helper acc' rws
   helper [] grid.toList
 
-
-def findSymmetry : Grid Tile → Nat := fun grid =>
-  match findRowSymmetry grid with
-  | some row => row * 100
-  | none => match findRowSymmetry grid.transpose with
-    | some col => col
-    | none => 0
+def findSymmetry (n : Nat) : Grid Tile → Nat := fun grid =>
+  let rowSyms := findRowSymmetry grid
+  match rowSyms.toArray.findIdx? (· = n) with
+  | some row => (row + 1) * 100
+  | none =>
+    let colSyms := findRowSymmetry grid.transpose
+    match colSyms.toArray.findIdx? (· = n) with
+    | some col => col + 1
+    | none => panic! "Did not find symmetry"
 
 
 def splitInto : List String → List (List String) := fun lns =>
@@ -70,32 +80,10 @@ def parseLines : List String → List (Grid Tile) := fun lns =>
   )
   List.map Grid.mk grids
 
-def input1 := [
-"#.##..##.",
-"..#.##.#.",
-"##......#",
-"##......#",
-"..#.##.#.",
-"..##..##.",
-"#.#.##.#.",
-"",
-"#...##..#",
-"#....#..#",
-"..##..###",
-"#####.##.",
-"#####.##.",
-"..##..###",
-"#....#..#",
-]
-
-def grids := parseLines input1
--- #eval Util.sum 0 (grids.map findSymmetry)
-
 def runDay : List String → String := fun lns =>
   let grids := parseLines lns
-  let sln1 := Util.sum 0 (grids.map findSymmetry)
-  let sln2 := 0
+  let sln1 := Util.sum 0 (grids.map (findSymmetry 0))
+  let sln2 := Util.sum 0 (grids.map (findSymmetry 1))
   s!"Part 1: {sln1}\nPart 2: {sln2}"
-
 
 end Day13
